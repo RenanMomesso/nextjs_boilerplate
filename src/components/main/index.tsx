bccb3cbe93711e382f90ddcca3db5ddd1268794f
Renan Momesso <Renan>
2020-12-13 21:40:31 -0300

    First Test

---
+++ src/components/main/index.tsx
@@ -0,0 +1,4 @@
+//import React from 'react';
+const Main = () => <main><h1>React Avançado</h1></main>
+
+export default Main
---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+
+
+interface Props {
+  title:string
+}
+
+export default function Home({ title = 'React Avançado' }:Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+       
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" />
+        </a>
+      </footer>
+    </div>
+  )
+}
